e223ca9500d188ed990479d90f03eb75d0036c52
mohammedalnaggar <mohammed.alnaggar93@gmail.com>
2023-04-05 17:44:26 +0200

    clean up

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
-resource "aws_instance" "bastion-host" {
-  ami           = "ami-007855ac798b5175e"
-  instance_type = "t3.nano"
+# resource "aws_instance" "bastion-host" {
+#   ami           = "ami-007855ac798b5175e"
+#   instance_type = "t3.nano"
 
 
-  key_name = "timeless-test"
+#   key_name = "timeless-test"
 
-  vpc_security_group_ids = [aws_security_group.bastion_host_sg.id]
+#   vpc_security_group_ids = [aws_security_group.bastion_host_sg.id]
 
-  tags = {
-    Name = "timeless-test"
-  }
-}
+#   tags = {
+#     Name = "timeless-test"
+#   }
+# }
 
-resource "aws_security_group" "bastion_host_sg" {
+# resource "aws_security_group" "bastion_host_sg" {
 
-  description = "Host SG"
-  name        = "bastion-host-sg"
-  vpc_id      = "vpc-00b7fd2a39f7ce98f"
+#   description = "Host SG"
+#   name        = "bastion-host-sg"
+#   vpc_id      = "vpc-00b7fd2a39f7ce98f"
 
-  # inbound rule for Bastion Host SSH
-  ingress {
-    description = "Bastion Host SG"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   # inbound rule for Bastion Host SSH
+#   ingress {
+#     description = "Bastion Host SG"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    description = "output from Bastion Host"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     description = "output from Bastion Host"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
